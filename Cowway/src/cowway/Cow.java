da9ef59108cad91437931c1a3c9f05dace8a37f8
MW-Potential <mwu2021@gmail.com>
2016-01-11 21:41:06 -0800

    More Cow Changes

--- Cowway/src/cowway/Cow.java
+++ Cowway/src/cowway/Cow.java
@@ -12,14 +12,46 @@ public class Cow extends FarmObject {
     public Cow(String name, int hunger, int age, int sicknessLevel, int x, int y) {
         super(x, y);
         Name = name;
-        hunger = hunger;
-        age = age;
-        sicknessLevel = sicknessLevel;
+        this.hunger = hunger;
+        this.age = age;
+        this.sicknessLevel = sicknessLevel;
     }
     
     public void Move() { 
+        System.out.println("Cow \"" + this.Name + "\"'s original position: (" + getX() + "," + getY() + ").");
         Random rand = new Random();
-        setX(rand.nextInt((5 - 1) + 1) + 1);
-        setY(rand.nextInt((5 - 1) + 1) + 1);
+        int XorY = rand.nextInt((1 - 0) + 1) + 0;
+        int upOrDown = rand.nextInt((1 - 0) + 1) + 0;
+        if (XorY == 0) {
+            if (upOrDown == 0) {
+                if (getX() != 0) {
+                    setX(getX() - 1);
+                }
+            } else {
+                if (getX() != 5) {
+                    setX(getX() + 1);
+                }
+            }
+        } else {
+            if (upOrDown == 0) {
+                if (getY() != 0) {
+                    setY(getY() - 1);
+                }
+            } else {
+                if (getY() != 5) {
+                    setY(getY() + 1);
+                }
+            }
+        }
+        System.out.println("Cow \"" + this.Name + "\"'s new position: (" + getX() + "," + getY() + ").");
     }
+    
+    public void farmObjectSetX(int x) {
+        setX(x);
+    }
+    
+    public void farmObjectSetY(int y) {
+        setY(y);
+    }
+    
 }
--- Cowway/src/cowway/FlyingCow.java
+++ Cowway/src/cowway/FlyingCow.java
@@ -6,10 +6,21 @@
 
 package cowway;
 
+import java.util.Random;
+
 /**
  *
  * @author Mitchell
  */
-public class FlyingCow {
+public class FlyingCow implements Cow {
     
+    public FlyingCow(String name, int hunger, int age, int sicknessLevel, int x, int y) {
+        super(name, hunger, age, sicknessLevel, x, y);
+    }
+    
+//    public void Move() {
+//        Random rand = new Random();
+//        farmObjectSetX(rand.nextInt((5 - 1) + 1) + 1);
+//        setY(rand.nextInt((5 - 1) + 1) + 1);        
+//    }
 }
